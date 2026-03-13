c0d06aa62b72d5e7daf5f0403c439bf3954ba1f8
PedroBianchini <Kgquentao@gmail.com>
2026-03-13 14:34:19 -0300

    Add files via upload

---
+++ att python.py
@@ -0,0 +1,29 @@
+
+'''
+lista = [7.5, 8.0, 6.0, 9.5, 5.0]
+lista.append(8.5)
+lista [4] = 6.5
+print (lista)
+print("Mínimo:", min(lista))
+print("Máximo:", max(lista)
+'''
+'''
+lista = ["heitor microwave", "pedro", "rai", "jeff","patolino"]
+
+for i, aluno in enumerate(lista, start=1):
+    print (f'Aluno {i}: {aluno}')
+'''
+
+numeros = [3, 17, 8, 42, 5, 100, 23, 66, 11, 99]
+
+'''
+for num in numeros:
+    if num % 2 == 0:
+        print(num)
+'''
+'''
+nova_lista = [n for n in numeros if n > 20]
+'''
+
+total = sum(numeros)
+print (total)
