bdda5fe3d7d916ec4dfc2ee20c1374d435f8a90f
PedroBianchini <Kgquentao@gmail.com>
2026-03-16 13:50:28 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,10 @@
+aluno = {
+    'nome': 'Pedro',
+    'matricula': 123456,
+    'turma': 'E',
+    'notas': [7.5, 8, 5]
+}
+
+print (aluno)
+print ("nome", aluno ['nome'])
+print ("notas:", aluno ['notas'])
