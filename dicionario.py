e49d1f4d08b03d3b4d19395ff8de157a52427d2a
PedroBianchini <Kgquentao@gmail.com>
2026-03-16 14:35:13 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,3 +1,5 @@
+DICIONARIO
+
 aluno = {
     'nome': 'Pedro',
     'matricula': 123456,
@@ -8,3 +10,33 @@ aluno = {
 print (aluno)
 print ("nome", aluno ['nome'])
 print ("notas:", aluno ['notas'])
+'''--------------------------'''
+'''
+contato = {
+    'nome': 'Pedro',
+    'telefone': 99996767,
+    'email': 'PaulinDaOnça@gmail.com',
+    'cidade': 'RioTiete'
+}
+
+print ("Itens do dicionario")
+print (contato.keys())
+'''
+'''--------------------'''
+
+contato = {
+    'nome': 'Pedro',
+    'telefone': 99996767,
+    'email': 'PaulinDaOnça@gmail.com',
+    'cidade': 'Rio Tiete',
+}
+
+contato['Instagram'] = 'Artur(irmao do mini pedro)'
+
+del contato['telefone']
+
+print(contato)
+print(contato.get('Instagram'))
+
+if 'email' in contato:
+    print("xave achada")
